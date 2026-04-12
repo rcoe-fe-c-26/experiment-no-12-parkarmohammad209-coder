8763fc923de3eae3196f3fb5fd1446c71c7d96db
parkarmohammad209-coder <parkarmohammad209@gmail.com>
2026-04-12 14:45:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # AIM: Develop a Python program that reads a text file and 
 # prints words of specified lengths (e.g., three, four, 
 # five, etc.) found within the file.
-# Coder:
+# Coder:Mohammed Parkar
 # Date:
 
 print("--- Extracting Words from Text File ---\n")
